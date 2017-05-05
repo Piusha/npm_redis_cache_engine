5247eb68634882aff9056d64c7ebc41b9ab39234
vagrant <vagrant@vagrant.vm>
2017-05-05 04:41:37 +0000

    Add Option to define redis cache db

--- RedisCacheEngine.js
+++ RedisCacheEngine.js
@@ -7,12 +7,30 @@ var RedisCacheEngine ={
      * @param host
      * @param port
      */
-	init:function(host,port){
+	init:function(host,port,db){
 		var CacheClient = require('redis');
-		this._cacheClient = CacheClient.createClient(port,host);
-        this._cacheClient.on('connect', function() {
-            console.log("CONNECTED TO CACHE_HOST ON :",host,port);
-        });
+        RedisCacheEngine._cacheClient = CacheClient.createClient(port,host);
+        if(typeof db == 'undefined'){
+            RedisCacheEngine._cacheClient.on('connect', function() {
+                console.log("CONNECTED TO CACHE_HOST ON :",host,port);
+            });
+        }else{
+            RedisCacheEngine._cacheClient.select(1,function(err,res){
+                if(err == null){
+                    RedisCacheEngine._cacheClient.set('redisdb', db);
+                    RedisCacheEngine._cacheClient.on('connect', function() {
+                        console.log("CONNECTED TO CACHE_HOST ON :",host,port);
+                    });
+                }else{
+                    console.log(err);
+
+                }
+
+
+            })
+        }
+
+
 
 	},
 
